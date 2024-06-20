5bb6b5f276692dc6bd7099eb35ba706af62be622
hyenasky <ashnwill49@gmail.com>
2024-06-20 01:51:34 -0600

    Bl: Initialize memory management, enable paging

--- OS/Loader/XRstation/OSLoader.mk
+++
@@ -1,8 +0,0 @@
-OUTPUTFILE := OSLoader.a4x
-
-LIBDIR := $(REPO)/../a4x/Headers:$(NEWSDK)/Rtl/Headers
-
-PRELIBS  := ./XRstation/BlXrComputer.$(ARCHITECTURE).$(CHKFRE).o
-LIBS     := $(NEWSDK)/Rtl/obj/$(ARCHITECTURE)/Rtl.lib
-MOVEEXPR := text=0x3200,data=text+text_size,bss=data+data_size
-MKBINARY := yes
